437e752fd99038dedc5e79f5b7e0da5d1ce295ec
Marius Gundersen <me@mariusgundersen.net>
2015-11-29 20:18:19 +0100

    startBeforeStop should handle not having anything to stop

--- src/deploy/startBeforeStop.js
+++ src/deploy/startBeforeStop.js
@@ -4,12 +4,14 @@ module.exports = co.wrap(function *(containerToStart, containerToStop, log){
   log('Starting new container')
   yield containerToStart.start();
   log('Container started');
-  log('Waiting 5 seconds');
-  yield delay(5000);
-  log('Waited 5 seconds');
-  log('Stopping previous container');
-  yield containerToStop.stop();
-  log('Container stopped');
+  if(containerToStop && 'stop' in containerToStop){
+    log('Waiting 5 seconds');
+    yield delay(5000);
+    log('Waited 5 seconds');
+    log('Stopping previous container');
+    yield containerToStop.stop();
+    log('Container stopped');
+  }
 });
 
 function delay(ms){
--- src/deploy/startBeforeStop.test.js
+++ src/deploy/startBeforeStop.test.js
@@ -6,7 +6,7 @@ describe("startBeforeStop", function(){
   it("should be a function", function(){
     startBeforeStop.should.be.a('Function');
   });
-  
+
   describe("when called", function(){
     beforeEach(function(){
       this.clock = sinon.useFakeTimers();
@@ -14,30 +14,56 @@ describe("startBeforeStop", function(){
       this.stopSpy = sinon.stub().returns(Promise.resolve());
       this.logSpy = sinon.spy(m => m == 'Waiting 5 seconds' && Promise.resolve().then(() => this.clock.tick(5000)));
     });
-    
+
     afterEach(function(){
       this.clock.restore();
     });
-    
+
     it("should do things in the right order", co.wrap(function*(){
-            
+
       yield startBeforeStop({start: this.startSpy}, {stop: this.stopSpy}, this.logSpy);
-      
+
       this.logSpy.should.have.been.calledWith('Starting new container');
-      
+
       this.startSpy.should.have.been.calledOnce;
-      
+
       this.logSpy.should.have.been.calledWith('Container started');
-      
+
       this.logSpy.should.have.been.calledWith('Waiting 5 seconds');
-      
+
       this.logSpy.should.have.been.calledWith('Waited 5 seconds');
-      
+
       this.logSpy.should.have.been.calledWith('Stopping previous container');
-      
+
       this.stopSpy.should.have.been.calledOnce;
-      
+
       this.logSpy.should.have.been.calledWith('Container stopped');
+    }));
+  });
+
+  describe("when called without any container to stop", function(){
+    beforeEach(function(){
+      this.clock = sinon.useFakeTimers();
+      this.startSpy = sinon.stub().returns(Promise.resolve());
+      this.stopSpy = sinon.spy();
+      this.logSpy = sinon.spy();
+    });
+
+    afterEach(function(){
+      this.clock.restore();
+    });
+
+    it("should do things in the right order", co.wrap(function*(){
+
+      yield startBeforeStop({start: this.startSpy}, null, this.logSpy);
+
+      this.logSpy.should.have.been.calledWith('Starting new container');
+
+      this.startSpy.should.have.been.calledOnce;
+
+      this.logSpy.should.have.been.calledWith('Container started');
+
+      this.stopSpy.should.not.have.been.called;
     }));
   });
 });
