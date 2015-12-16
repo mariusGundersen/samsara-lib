bc1fdb9fe7b401df4f8878e8dd658a2ad68f4d3e
Marius Gundersen <me@mariusgundersen.net>
2015-12-16 23:33:51 +0100

    Created a fileLogger

---
+++ src/deploy/fileLogger.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const co = require('co');
+const fs = require('fs-promise');
+const mkdirp = require('mkdirp-promise');
+const paths = require('../paths');
+
+module.exports = function(eventEmitter){
+  let plan;
+  let setLogStream;
+  const logStream = new Promise(function(resolve){
+    setLogStream = resolve;
+  });
+
+  eventEmitter.on('start', co.wrap(function*(event){
+    try{
+      plan = event.plan;
+      const name = event.spirit;
+      const life = event.life+'';
+      yield mkdirp(paths.life(name, life));
+      const config = JSON.stringify(event.config, null, '  ');
+      yield fs.writeFile(paths.spiritLifeConfig(name, life), config);
+      const stream = fs.createWriteStream(paths.spiritLifeDeployLog(name, life));
+      stream.write(pad('deploy')+'\n');
+      setLogStream(stream);
+    }catch(e){
+      console.error(e);
+    }
+  }));
+
+  eventEmitter.on('stage', co.wrap(function*(event){
+    const stream = yield logStream;
+    const stage = plan.shift();
+    stream.write(pad(stage)+'\n');
+  }));
+
+  eventEmitter.on('message', co.wrap(function*(event){
+    const stream = yield logStream;
+    if(typeof(event.message) == 'string'){
+      stream.write(event.message + '\n');
+    }else if(event.message.progress){
+      stream.write(event.message.id + ': ' + event.message.status + prettyProgress(event.message.progresseDetail) + '\n');
+    }
+  }));
+
+  eventEmitter.on('stop', co.wrap(function*(event){
+    const stream = yield logStream;
+    stream.write(pad(event.error ? 'failed' : 'done')+'\n');
+    if(event.error){
+      stream.write(event.error.message);
+    }
+    stream.end();
+  }));
+};
+
+function prettyProgress(progress){
+  if(progress != null && typeof(progress) == 'object' && 'current' in progress && 'total' in progress){
+    const percent = (progress.current/progress.total*100).toFixed(0);
+    const mib = (progress.total/1024/1024).toFixed(2);
+    return `(${percent}% of ${mib}MiB)`;
+  }
+
+  return '';
+}
+
+function pad(name){
+  const padWith = 50 - 4 - name.length;
+  let result = '';
+  for(var i=0; i<padWith/2; i++){
+    result+='=';
+  }
+  result+= '[ '+name+' ]';
+  for(; i<padWith; i++){
+    result+='=';
+  }
+  return result;
+}
---
+++ src/deploy/fileLogger.test.js
@@ -0,0 +1,57 @@
+const fileLogger = require('./fileLogger');
+const sinon = require('sinon');
+const co = require('co');
+const descartes = require('descartes');
+const fs = require('fs-promise');
+const mkdirp = require('mkdirp-promise');
+
+describe("fileLogger", function(){
+  it("should be a function", function(){
+    fileLogger.should.be.a('Function');
+  });
+
+  describe("when called", function(){
+    beforeEach(function(){
+      const jar = this.jar = new descartes.Jar();
+      this.clock = sinon.useFakeTimers();
+      this.onSpy = jar.sensor('eventEmitter.on');
+      this.createWriteStreamSpy = jar.probe('createWriteStream');
+      this.writeFileSpy = jar.probe('writeFile');
+      this.writeSpy = jar.sensor('fileStream.write');
+      this.endSpy = jar.sensor('fileStream.end');
+      sinon.stub(fs, 'createWriteStream', this.createWriteStreamSpy);
+      sinon.stub(fs, 'writeFile', this.writeFileSpy);
+    });
+
+    afterEach(function(){
+      this.clock.restore();
+      fs.createWriteStream.restore();
+      fs.writeFile.restore();
+    });
+
+    it("should do things in the right order", co.wrap(function*(){
+      const result = fileLogger({on: this.onSpy});
+
+      const startCall = yield this.onSpy.called(descartes.withArgs('start'));
+      const stageCall = yield this.onSpy.called(descartes.withArgs('stage'));
+      const messageCall = yield this.onSpy.called(descartes.withArgs('message'));
+      const stopCall = yield this.onSpy.called(descartes.withArgs('stop'));
+
+      const onStart = startCall.args[1];
+      const onStage = stageCall.args[1];
+      const onMessage = messageCall.args[1];
+      const onStop = stopCall.args[1];
+
+      onStart({spirit: 'test', life: 12, config: {name: 'test'}});
+
+      yield this.writeFileSpy.called(descartes.withArgs('config/spirits/test/lives/12/config.json', '{\n  "name": "test"\n}'));
+
+      this.createWriteStreamSpy.resolves({write: this.writeSpy, end: this.endSpy});
+      yield this.createWriteStreamSpy.called(descartes.withArgs('config/spirits/test/lives/12/deploy.log'));
+
+      yield this.writeSpy.called();
+
+      this.jar.done();
+    }));
+  });
+});
