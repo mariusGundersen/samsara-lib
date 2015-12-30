c0f79d49cf66293f4ef01c1a80e078e0c5455b62
Marius Gundersen <me@mariusgundersen.net>
2015-12-30 18:57:05 +0100

    When creating a new spirit the container config should be saved as yaml

--- src/createSpirit.js
+++ src/createSpirit.js
@@ -1,6 +1,7 @@
 const co = require('co');
 const fs = require('fs-promise');
 const mkdirp = require('mkdirp-promise');
+const yaml = require('js-yaml');
 const pathTo = require('./paths');
 
 module.exports = co.wrap(function *(name, image, tag){
@@ -12,23 +13,17 @@ module.exports = co.wrap(function *(name, image, tag){
     url: '',
     webhook: {
       enable: false,
-      secret: ''
+      secret: '',
+      matchTag: ''
     }
   };
 
   const containerConfig = {
-    image: image,
-    tag: tag,
-    raw: {},
-    env: {},
-    links: {},
-    ports: {},
-    volumes: {},
-    volumesFrom: []
+    image: image+':'+tag
   };
 
   yield mkdirp(pathTo.spirit(name));
   yield mkdirp(pathTo.spiritLives(name));
-  yield fs.writeFile(pathTo.spiritContainerConfigJson(name), JSON.stringify(containerConfig, null, '  '));
+  yield fs.writeFile(pathTo.spiritContainerConfig(name), yaml.safeDump({[name]:containerConfig}));
   yield fs.writeFile(pathTo.spiritSettingsJson(name), JSON.stringify(spiritSettings, null, '  '));
 });
