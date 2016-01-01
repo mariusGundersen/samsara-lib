2bf40811a1c8fe14643f4be3d748c808013d5655
Marius Gundersen <me@mariusgundersen.net>
2016-01-01 15:45:14 +0100

    Created a class for managing the container config

---
+++ src/ContainerConfig.js
@@ -0,0 +1,172 @@
+'use strict'
+
+const fs = require('fs-promise');
+const pathTo = require('./paths');
+const yaml = require('js-yaml');
+
+module.exports = class ContainerConfig{
+  constructor(name, fileContent){
+    this._name = name;
+    this._fileContent = fileContent;
+    this.yaml = yaml.safeLoad(fileContent);
+  }
+  get config(){
+    return this.yaml[this._name];
+  }
+  set config(value){
+    this.yaml[this._name] = value;
+  }
+  get image(){
+    return (this.config.image || '').split(':')[0];
+  }
+  set image(value){
+    this.config.image = value + ':' + this.tag;
+  }
+  get tag(){
+    return (this.config.image || '').split(':')[1] || 'latest';
+  }
+  set tag(value){
+    this.config.image = this.image + ':' + value;
+  }
+  get environment(){
+    if(Array.isArray(this.config.environment)){
+      return this.config.environment
+        .map(env => env.split('='))
+        .map(pair => ({
+          key: pair.shift(),
+          value: pair.join('=')
+        }));
+    }else{
+      return Object.keys(this.config.environment || {})
+        .map(key => ({
+          key: key,
+          value: this.config.environment[key]
+        }));
+    }
+  }
+  set environment(value){
+    this.config.environment = value
+      .map(env => env.key+'='+env.value);
+  }
+  get volumes(){
+    return (this.config.volumes || [])
+      .map(volume => volume.split(':'))
+      .map(parts => ({
+        containerPath: parts[parts.length == 1 ? 0 : 1],
+        hostPath: parts.length == 1 ? '' : parts[0],
+        readOnly: parts.length == 3 ? parts[2] === 'ro' : false
+      }));
+  }
+  set volumes(value){
+    this.config.volumes = value
+      .map(volume => {
+        if(volume.hostPath){
+          if(volume.readOnly){
+            return volume.hostPath+':'+volume.containerPath+':ro';
+          }else{
+            return volume.hostPath+':'+volume.containerPath;
+          }
+        }else{
+          if(volume.readOnly){
+            return volume.containerPath+':ro';
+          }else{
+            return volume.containerPath;
+          }
+        }
+      });
+  }
+  get ports(){
+    return (this.config.ports || [])
+      .map(port => {
+        const parts = port.split(':');
+        if(parts.length == 1){
+          return {containerPort: parts[0], hostPort: '', hostIp: ''};
+        }else if(parts.length == 2){
+          return {containerPort: parts[1], hostPort: parts[0], hostIp: ''};
+        }else if(parts.length == 3){
+          return {containerPort: parts[2], hostPort: parts[1], hostIp: parts[0]};
+        }
+      });
+  }
+  set ports(value){
+    this.config.ports = value
+      .map(port => {
+        if(port.hostPort){
+          if(port.hostIp){
+            return port.hostIp+':'+port.hostPort+':'+port.containerPort;
+          }else{
+            return port.hostPort+':'+port.containerPort;
+          }
+        }else{
+          return port.containerPort;
+        }
+      });
+  }
+  get links(){
+    return (this.config.links || [])
+      .map(link => {
+        const parts = link.split(':');
+        const match = /^spirit\(([a-zA-Z0-9_\.-]+)\)$/.exec(parts[0]);
+        if(match){
+          if(parts.length == 1){
+            return {spirit: match[1], alias: match[1], container: ''};
+          }else if(parts.length == 2){
+            return {spirit: match[1], alias: parts[1], container: ''};
+          }
+        }else{
+          if(parts.length == 1){
+            return {container: parts[0], alias: parts[0], spirit: ''};
+          }else if(parts.length == 2){
+            return {container: parts[0], alias: parts[1], spirit: ''};
+          }
+        }
+      });
+  }
+  set links(value){
+    this.config.links = value
+      .map(link => {
+        if(link.spirit){
+          return `spirit(${link.spirit}):${link.alias}`;
+        }else{
+          return `${link.container}:${link.alias}`;
+        }
+      });
+  }
+  get volumesFrom(){
+    return (this.config.volumesFrom || [])
+      .map(volumeFrom => {
+        const parts = volumeFrom.split(':');
+        const match = /^spirit\(([a-zA-Z0-9_\.-]+)\)$/.exec(parts[0]);
+        if(match){
+          if(parts.length == 1){
+            return {spirit: match[1], readOnly: false, container: ''};
+          }else if(parts.length == 2){
+            return {spirit: match[1], readOnly: parts[1] === 'ro', container: ''};
+          }
+        }else{
+          if(parts.length == 1){
+            return {container: parts[0], readOnly: false, spirit: ''};
+          }else if(parts.length == 2){
+            return {container: parts[0], readOnly: parts[1] === 'ro', spirit: ''};
+          }
+        }
+      });
+  }
+  set volumesFrom(value){
+    this.config.volumesFrom = value
+      .map(volumeFrom => {
+        if(volumeFrom.spirit){
+          return `spirit(${volumeFrom.spirit})`+(volumeFrom.readOnly ? ':ro' : '');
+        }else{
+          return volumeFrom.container + (volumeFrom.readOnly ? ':ro' : '');
+        }
+      });
+  }
+  save(){
+    const output = yaml.safeDump(this.yaml);
+    return fs.writeFile(pathTo.spiritContainerConfig(this._name), output);
+  }
+  toString(){
+    return this._fileContent;
+  }
+};
---
+++ src/ContainerConfig.test.js
@@ -0,0 +1,276 @@
+'use strict'
+
+const co = require('co');
+const ContainerConfig = require('./ContainerConfig');
+const sinon = require('sinon');
+const fs = require('fs-promise');
+const u = require('./util/unindent');
+
+describe.only("ContainerConfig", function() {
+  it("should toString with the file contents", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+    `);
+    containerConfig.toString().should.equal("test:\n  image: 'nginx:latest'\n");
+  });
+
+  it("should deserialize the file contents", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+    `);
+    containerConfig.yaml.should.deep.equal({test:{image: 'nginx:latest'}});
+  });
+
+  it("should provide the environment variables as an array", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+        environment:
+          - VIRTUAL_HOST=mariusgundersen.net
+    `);
+
+    containerConfig.environment.should.deep.equal([
+      {key: 'VIRTUAL_HOST', value: 'mariusgundersen.net'}
+    ]);
+  });
+
+  it("should provide the image", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+    `);
+
+    containerConfig.image.should.equal('nginx');
+  });
+
+  it("should provide the tag", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+    `);
+
+    containerConfig.tag.should.equal('latest');
+  });
+
+  it("should provide the tag even when it is missing", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx'
+    `);
+
+    containerConfig.tag.should.equal('latest');
+  });
+
+  it("should provide the environment variables as an array even when it's an object", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+        environment:
+          VIRTUAL_HOST: mariusgundersen.net
+    `);
+
+    containerConfig.environment.should.deep.equal([
+      {key: 'VIRTUAL_HOST', value: 'mariusgundersen.net'}
+    ]);
+  });
+
+  it("should provide the volumes", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+        volumes:
+          - '/container/path'
+          - '/host/path:/container/path'
+          - 'volume-name:/container/path:ro'
+    `);
+
+    containerConfig.volumes.should.deep.equal([
+      {hostPath: '', containerPath: '/container/path', readOnly: false},
+      {hostPath: '/host/path', containerPath: '/container/path', readOnly: false},
+      {hostPath: 'volume-name', containerPath: '/container/path', readOnly: true}
+    ]);
+  });
+
+  it("should provide the ports", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+        ports:
+          - '80'
+          - '8080:80'
+          - '127.0.0.1:8080:80'
+    `);
+
+    containerConfig.ports.should.deep.equal([
+      {containerPort: '80', hostPort: '', hostIp: ''},
+      {containerPort: '80', hostPort: '8080', hostIp: ''},
+      {containerPort: '80', hostPort: '8080', hostIp: '127.0.0.1'}
+    ]);
+  });
+
+  it("should provide the links", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+        links:
+          - 'service'
+          - 'service:alias'
+          - 'spirit(service)'
+          - 'spirit(service):alias'
+    `);
+
+    containerConfig.links.should.deep.equal([
+      {container: 'service', spirit: '', alias: 'service'},
+      {container: 'service', spirit: '', alias: 'alias'},
+      {container: '', spirit: 'service', alias: 'service'},
+      {container: '', spirit: 'service', alias: 'alias'}
+    ]);
+  });
+
+  it("should provide the volumesFrom", function(){
+    const containerConfig = new ContainerConfig('test', u`
+      test:
+        image: 'nginx:latest'
+        volumesFrom:
+          - 'service'
+          - 'service:ro'
+          - 'spirit(service)'
+          - 'spirit(service):ro'
+    `);
+
+    containerConfig.volumesFrom.should.deep.equal([
+      {container: 'service', spirit: '', readOnly: false},
+      {container: 'service', spirit: '', readOnly: true},
+      {container: '', spirit: 'service', readOnly: false},
+      {container: '', spirit: 'service', readOnly: true}
+    ]);
+  });
+
+  describe("setters", function(){
+    let basicConfig;
+
+    beforeEach(function(){
+      basicConfig = new ContainerConfig('test', 'test: {}\n');
+    });
+
+    it("should set the config", function(){
+      basicConfig.config = {
+        image: 'nginx:1.7'
+      };
+
+      basicConfig.config.should.deep.equal({
+        image: 'nginx:1.7'
+      });
+    });
+
+    it("should set the image", function(){
+      basicConfig.image = 'nginx';
+
+      basicConfig.config.should.deep.equal({
+        image: 'nginx:latest'
+      });
+    });
+
+    it("should set the image and tag", function(){
+      basicConfig.image = 'nginx';
+      basicConfig.tag = '1.7'
+
+      basicConfig.config.should.deep.equal({
+        image: 'nginx:1.7'
+      });
+    });
+
+    it("should set the environment", function(){
+      basicConfig.environment = [
+        {key: 'VIRTUAL_HOST', value:'mariusgundersen.net'}
+      ];
+
+      basicConfig.config.environment.should.deep.equal([
+        'VIRTUAL_HOST=mariusgundersen.net'
+      ]);
+    });
+
+    it("should set the volumes", function(){
+      basicConfig.volumes = [
+        {hostPath: '', containerPath: '/container/path', readOnly: false},
+        {hostPath: '/host/path', containerPath: '/container/path', readOnly: false},
+        {hostPath: 'volume-name', containerPath: '/container/path', readOnly: true}
+      ];
+
+      basicConfig.config.volumes.should.deep.equal([
+        '/container/path',
+        '/host/path:/container/path',
+        'volume-name:/container/path:ro'
+      ]);
+    });
+
+    it("should set the ports", function(){
+      basicConfig.ports = [
+        {containerPort: '80', hostPort: '', hostIp: ''},
+        {containerPort: '80', hostPort: '8080', hostIp: ''},
+        {containerPort: '80', hostPort: '8080', hostIp: '127.0.0.1'}
+      ];
+
+      basicConfig.config.ports.should.deep.equal([
+        '80',
+        '8080:80',
+        '127.0.0.1:8080:80'
+      ]);
+    });
+
+    it("should set the links", function(){
+      basicConfig.links = [
+        {container: 'service', spirit: '', alias: 'service'},
+        {container: 'service', spirit: '', alias: 'alias'},
+        {container: '', spirit: 'service', alias: 'service'},
+        {container: '', spirit: 'service', alias: 'alias'}
+      ];
+
+      basicConfig.config.links.should.deep.equal([
+        'service:service',
+        'service:alias',
+        'spirit(service):service',
+        'spirit(service):alias'
+      ]);
+    });
+
+    it("should set the volumesFrom", function(){
+      basicConfig.volumesFrom = [
+        {container: 'service', spirit: '', readOnly: false},
+        {container: 'service', spirit: '', readOnly: true},
+        {container: '', spirit: 'service', readOnly: false},
+        {container: '', spirit: 'service', readOnly: true}
+      ];
+
+      basicConfig.config.volumesFrom.should.deep.equal([
+        'service',
+        'service:ro',
+        'spirit(service)',
+        'spirit(service):ro'
+      ]);
+    });
+  });
+
+  describe("saving", function(){
+    beforeEach(function(){
+      sinon.stub(fs, 'writeFile').returns(Promise.resolve());
+    });
+
+    afterEach(function(){
+      fs.writeFile.restore();
+    });
+
+    it("should write the contents of the config to the right file", co.wrap(function *(){
+      const containerConfig = new ContainerConfig('test', u`
+        test:
+          image: 'nginx:latest'
+      `);
+
+      yield containerConfig.save();
+
+      fs.writeFile.should.have.been.calledWith('config/spirits/test/containerConfig.yml', "test:\n  image: 'nginx:latest'\n");
+    }));
+  });
+});
