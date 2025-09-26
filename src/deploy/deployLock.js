ddc90e910c1fafadfaf8874ffdc14c189dc85628
Marius Gundersen <me@mariusgundersen.net>
2025-09-26 14:44:47 +0200

    Update deployLock.js

--- src/deploy/deployLock.js
+++ src/deploy/deployLock.js
@@ -3,7 +3,7 @@ import { spiritDeployLock } from "../paths.js";
 
 export async function lock(name) {
   const file = await fs.open(spiritDeployLock(name), "wx");
-  fs.writeFile(file, new Date().toISOString(), 0, "utf8");
+  fs.write(file, new Date().toISOString(), 0, "utf8");
 }
 export async function unlock(name) {
   fs.unlink(spiritDeployLock(name));
